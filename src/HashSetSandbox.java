036ec718256278674da6d31f43b04bde943890df
jordanhope <jordanbhope@outlook.com>
2015-09-23 11:52:20 -0600

    add HashSetSandbox & LinkedHashSandbox

---
+++ src/HashSetSandbox.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by JordanHope on 9/22/2015.
+ */
+public class HashSetSandbox {
+    public static void main(String args[]) {
+        /* use a hashset to store elements when order does not matter and when
+         * you do not want duplicates in your set of data
+         */
+        Set set = new HashSet();
+
+        String element = "element 1";
+
+        set.add(element);
+        set.add("Is");
+        set.add("My");
+        set.add(2.1);
+        set.add(3);
+        set.add('a');
+
+        System.out.println(set);
+
+        //nasty path element null
+
+        Set setA = new HashSet();
+
+        String element1 = null;
+
+        setA.add(element1);
+
+        System.out.println(setA);
+
+        setA.add(null);
+
+        System.out.println(setA);
+
+        // determine the type of set
+        HashSet<String> setB = new HashSet<>();
+
+        setB.add("3");
+        setB.add("Hello");
+
+        System.out.println(setB);
+
+        setB.remove("3");
+
+        System.out.println(setB);
+    }
+
+
+}
---
+++ src/LinkedHashSetSandbox.java
@@ -0,0 +1,29 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by JordanHope on 9/22/2015.
+ */
+public class LinkedHashSetSandbox {
+    public static void main(String args[])
+    {
+        /*maintains a list of entries in the set, in the order in which they were inserted.
+        hybrid of a hash table and a linked list. does not accept duplicates
+        */
+        Set lhSet = new LinkedHashSet();
+
+
+        String element = "element 1";
+
+        lhSet.add(element);
+        lhSet.add("Is");
+        lhSet.add("My");
+        lhSet.add(2.1);
+        lhSet.add(3);
+        lhSet.add('a');
+
+        System.out.println(lhSet);
+
+
+    }
+}
