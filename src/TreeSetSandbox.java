be370ea4015b2660eec8fce0293b5b55146f2622
jordanhope <jordanbhope@outlook.com>
2015-09-24 12:26:53 -0600

    revised and added to TreeSetSandbox

--- src/TreeSetSandbox.java
+++ src/TreeSetSandbox.java
@@ -8,7 +8,12 @@ public class TreeSetSandbox {
     public static void main(String args[]) {
         /*
          * TreeSet will order elements in the set, does not accept duplicates.
+         * TreeSet has to either work with the same data type or has to be passed a
+         * comparator. A comparator is a function that handles how different data types can be compared
+         * example, a string should become before a char, but after int.
          */
+
+        try {
         Set set = new TreeSet();
 
         set.add("element");
@@ -18,6 +23,47 @@ public class TreeSetSandbox {
         set.add("3");
         set.add("a");
 
-        System.out.println(set);
+            System.out.println(set);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Set setB = new TreeSet();
+
+            setB.add(1);
+            setB.add(5);
+            setB.add(3);
+            setB.add(2);
+            setB.add(4);
+            setB.add(1);
+            setB.add("five");
+
+
+            System.out.println(setB);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Set setC = new TreeSet();
+
+            int a = 1;
+            int b = 2;
+            int c = 250000000;
+            int d = 3;
+            int e = 99999999;
+
+            setC.add(a);
+            setC.add(b);
+            setC.add(c);
+            setC.add(d);
+            setC.add(e);
+
+            System.out.println(setC);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+
     }
 }
