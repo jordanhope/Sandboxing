44a2eb4173699ae25cb5d42b34bcda7e073496ed
jordanhope <jordanbhope@outlook.com>
2015-10-28 11:38:13 -0600

    10.28.15  Updated happy and nasty paths

--- src/HashMapSandbox.java
+++ src/HashMapSandbox.java
@@ -2,16 +2,15 @@
  * Created by JordanHope on 9/22/2015.
  */
 import java.util.HashMap;
-import java.util.Map;
-
-
 
 public class HashMapSandbox {
 
     public static void main(String[] args) {
 
-        // this is the happy path
-        HashMap<Integer, String> map = new HashMap<>();
+        /**********************************
+         * HAPPY PATH
+         **********************************/
+        HashMap map = new HashMap();
 
         map.put(1, "One");
         map.put(2, "Two");
@@ -20,24 +19,63 @@ public class HashMapSandbox {
         map.put(5, "Five");
         map.put(6, "Six");
 
-        // this allows the keys and the values to be printed out
-        // next to each other, sort of like a look up table
-        for(Map.Entry<Integer, String> entry: map.entrySet()) {
-            int key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ": " + value);
+        System.out.println("HashMap of int key 1 - 6, string words one - six");
+        System.out.println(map);
+
+        System.out.println("Using remove to remove key 4");
+        map.remove(4);
+        System.out.println(map);
+
+        System.out.println("Using containsKey, returns bool");
+        System.out.println("map contains the key 3: " + map.containsKey(3));
+        System.out.println("map contains the key 4: " + map.containsKey(4));
+
+        System.out.println("Using containsValue, returns bool");
+        System.out.println("map contains the value Three: " + map.containsValue("Three"));
+        System.out.println("map contains the value Four: " + map.containsValue("Four"));
+
+        System.out.println("Using size()");
+        System.out.println("The size of map is: " + map.size());
+
+        System.out.println("Using values(), returns a Collection of the values in the map");
+        System.out.println(map.values());
+
+        System.out.println("Using keySet() returns the keys in a Set");
+        System.out.println(map.keySet());
+
+        System.out.println("Using entrySet(), returns the values in the map in a Set");
+        System.out.println(map.entrySet());
+
+        System.out.println("Using get(1), returns the element with the associated key.");
+        System.out.println(map.get(1));
+
+        System.out.println();
+
+        System.out.println("*****NASTY PATHS*****");
+        System.out.println();
+
+        System.out.println("Adding a value when the key is 7 and the value is null");
+        try {
+            map.put(7, null);
+            System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        // nasty path value is null
-        map.put(7, null);
-        System.out.println(map);
+        System.out.println("Adding a value when key is null");
+        try {
+            map.put(null, "Test");
+            System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        // nasty path key is null
-        map.put(null, "Test");
-        System.out.println(map);
+        System.out.println("using get for a key that does not exist");
+        try {
+            System.out.println(map.get(45));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        // nasty path use get for a key that doesn't exist
-        String result = map.get(45);
-        System.out.println(result);
     }
 }
